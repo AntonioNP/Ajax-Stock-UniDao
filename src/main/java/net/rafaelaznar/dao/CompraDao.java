cfa07c34b267b09c7c50b03c0761ca8afd482f72
al037665 <al037665@s09-pc08>
2014-01-16 12:19:07 +0100

    Unificar las clases Dao

--- src/main/java/net/rafaelaznar/dao/CompraDao.java
+++ src/main/java/net/rafaelaznar/dao/CompraDao.java
@@ -5,11 +5,16 @@
 package net.rafaelaznar.dao;
 
 import net.rafaelaznar.bean.CompraBean;
+import net.rafaelaznar.helper.Conexion;
 
 /**
  *
  * @author rafa
  */
-public interface CompraDao extends GenericDao<CompraBean> {
+
+public class CompraDao extends GenericDaoImplementation<CompraBean> {
     
+    public CompraDao(Conexion.Tipo_conexion tipo_conexion) throws Exception{
+        super(tipo_conexion, "compra");
+    }
 }
